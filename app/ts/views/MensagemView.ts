6bee4723ea6a667ef54e91aba0da9a395d002328
kevyn <kevynredlotus@gmail.com>
2021-02-04 10:30:05 -0300

    - namespace with classes

--- app/ts/views/MensagemView.ts
+++ app/ts/views/MensagemView.ts
@@ -1,7 +1,9 @@
-class MensagemView extends View<string> {
+namespace Views {
+  export class MensagemView extends Views.View<string> {
 
-  template(model: string): string {
-    return `<p class="alert alert-info">Negociação adicionada com sucesso!</p>`
+    template(model: string): string {
+      return `<p class="alert alert-info">Negociação adicionada com sucesso!</p>`
+    }
+    
   }
-  
 }
--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -4,17 +4,22 @@
 //pode setar <T> como a classe Negociacoes, como ela desejar
 //classe abstrata não pode ser instanciada, apenas herdada
 //jQuery vai ajudar a rodar o código em celulares muito antigos
-abstract class View<T> {
-  private _elemento: JQuery;
 
-  constructor(selector: string) {
-    this._elemento = $(selector)
-  }
+namespace Views { //Com isso, pode-se digitar 'Views.' e aparece 
+  export abstract class View<T> {
+    private _elemento: JQuery;
 
-  update(model: T) {
-    this._elemento.html(this.template(model))
+    constructor(selector: string) {
+      this._elemento = $(selector)
+    }
+
+    update(model: T) {
+      this._elemento.html(this.template(model))
+    }
+    //método abstrato é um método que não tem conteúdo, porém
+    //a classe que herda como filho, pode chamar ela e codificar como quiser
+    abstract template(model: T): string 
   }
-  //método abstrato é um método que não tem conteúdo, porém
-  //a classe que herda como filho, pode chamar ela e codificar como quiser
-  abstract template(model: T): string 
 }
+
+
